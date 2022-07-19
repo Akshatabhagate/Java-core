7126717419c43d28356d4026b080583c06870102
Akshatabhagate <108929118+Akshatabhagate@users.noreply.github.com>
2022-07-19 18:24:41 +0530

    Add files via upload

---
+++ Example2.java
@@ -0,0 +1,12 @@
+class Example2
+{
+	public static void main(String[] ar)
+
+        {
+          
+          for(int i=10;i>6;i--)
+            {
+              System.out.print(i);
+             }
+        } 
+}
